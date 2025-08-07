20e280aa371aaaa7fc6fd4f92bfa94b3a72363d7
vaibhav-jani <vaibhavaniljani@gmail.com>
2025-08-07 23:29:41 +0530

    Upgraded gradle project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.ai.engg.curves.x.y"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ai.engg.curves.x.y"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
